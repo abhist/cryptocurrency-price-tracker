7410cec1e2bf74f549abe6fd7ce6d18df69fd2f4
Abhist Chauhan <abhist.chauhan@eagleview.com>
2020-09-22 12:26:47 +0530

    xyz

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <h1>ABHIST</h1>
+        <h1>ABassafsIST</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
