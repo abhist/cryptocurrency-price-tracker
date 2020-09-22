ea96e053e87b0cd673c7888e0e7f73266b252483
Abhist Chauhan <abhist.chauhan@eagleview.com>
2020-09-22 12:19:51 +0530

    Updates

---
+++ precache-manifest.bf8e25c3c64367918e5803706a547032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6934099b9ff41e0ebdfa540bd1a54e1",
+    "url": "/cryptocurrency-price-tracker/index.html"
+  },
+  {
+    "revision": "c6e2ea0cdd5827ddc276",
+    "url": "/cryptocurrency-price-tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/cryptocurrency-price-tracker/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cryptocurrency-price-tracker/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e2ea0cdd5827ddc276",
+    "url": "/cryptocurrency-price-tracker/static/js/main.b77d7353.chunk.js"
+  },
+  {
+    "revision": "e89389c7a8931ea59393",
+    "url": "/cryptocurrency-price-tracker/static/js/runtime-main.7f266eb6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cryptocurrency-price-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
