b1e99f07c52b209cefc4ade1e26f9e7d7ef3af48
Abhist Chauhan <abhist.chauhan@eagleview.com>
2020-09-22 12:29:13 +0530

    abcd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <h1>TANISI</h1>
+        <h1>TANIS</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
